112a80998302b4c515e1299ebf81a9cf06ed194e
Ryan Slade <ryanslade@gmail.com>
2012-09-20 22:28:14 +0100

    Cleaned up logging and monitor the working directory

--- goautotest.go
+++ goautotest.go
@@ -16,10 +16,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = watcher.Watch(".")
+	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err = watcher.Watch(wd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer watcher.Close()
 
 	for {
 		select {
@@ -28,29 +35,28 @@ func main() {
 				cmd := exec.Command("go", "test")
 				stdout, err := cmd.StdoutPipe()
 				if err != nil {
-					log.Println("Error: ", err)
+					log.Println(err)
 					break
 				}
 
 				err = cmd.Start()
 				if err != nil {
-					log.Println("Error running test: ", err)
+					log.Println(err)
 					break
 				}
 
 				go io.Copy(os.Stdout, stdout)
 				err = cmd.Wait()
 				if err != nil {
-					log.Println("Error:", err)
+					log.Println(err)
 				}
 
 				fmt.Println()
 			}
 
 		case err := <-watcher.Error:
-			log.Println("Error:", err)
+			log.Println(err)
 		}
 	}
 
-	watcher.Close()
 }
